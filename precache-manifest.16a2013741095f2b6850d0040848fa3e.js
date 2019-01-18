37b0ee81713c579eed46c30ad23d5fd09af6d50b
Praveenchouhan0 <46786753+Praveenchouhan0@users.noreply.github.com>
2019-01-18 23:46:57 +0530

    first commit

---
+++ precache-manifest.16a2013741095f2b6850d0040848fa3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc66c59191e3c6551c6d",
+    "url": "/static/js/main.dc66c591.chunk.js"
+  },
+  {
+    "revision": "151e7a0df9a27dfc148c",
+    "url": "/static/js/1.151e7a0d.chunk.js"
+  },
+  {
+    "revision": "dc66c59191e3c6551c6d",
+    "url": "/static/css/main.34512f6a.chunk.css"
+  },
+  {
+    "revision": "151e7a0df9a27dfc148c",
+    "url": "/static/css/1.22fa53a4.chunk.css"
+  },
+  {
+    "revision": "837164c3a2f7050d90eb1d9d1916a1ce",
+    "url": "/index.html"
+  }
+];
